3990704dd80e24b9dcbc989d0807a6e03519b21d
Alyssa Wei <wei.alyssa@gmail.com>
2019-04-02 18:24:42 -0700

    Includes helper function that prints all elements with className

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,41 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// You should use document.body, element.childNodes, and element.classList
+
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  //Initialize variables
+  var resultsArray = [];
+
+  console.log(document.body.childNodes);
+
+  // If no body element, return null
+  if (document.body === null) {
+    return null;
+  } else {
+    // recursive helper function
+    searchChildNodes(document.body, className);
+  }
+  return resultsArray;
 };
+
+
+// Recursive helper function to search childNodes as long as nodeList  length > 0
+var searchChildNodes = function (element, className) {
+  // console.log("Child Nodes:" + element.childNodes);
+  // console.log("NodeList Length:" + element.childNodes.length);
+  // console.log("Element:" + element);
+  // console.log("Class List:" + element.classList);
+  // console.log("Class Name:" + className);
+  // console.log(element.classList+"" === className);
+
+  if (element.classList+"" === className) {
+    console.log(element); // element
+  }
+  for (var i = 0; i < element.childNodes.length; i++){
+    searchChildNodes(element.childNodes[i], className);
+  }
+}
