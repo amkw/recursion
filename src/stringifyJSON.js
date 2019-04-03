6561966cc69bbd6eba8645761422585ec00717fb
Alyssa Wei <wei.alyssa@gmail.com>
2019-04-02 19:09:34 -0700

    Return strings for simple cases (numbers, booleans, strings, null)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,17 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  console.log(typeof(obj));
+  console.log("Stringify:" + JSON.stringify(obj));
+
+  // If obj is a simple value, add value to empty string and return
+  if (typeof(obj !== "object")) {
+    if (typeof(obj) === "string") {
+      // console.log(typeof(obj));
+      return '"' + obj + '"';
+    }
+    return "" + obj;
+  } else {
+    // recursive helper function for objects
+  }
 };
